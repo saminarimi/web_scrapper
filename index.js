2673c877d605eb6ba5595a27082a453258d06c8c
Samina Rimi <saminaEM2021@gmail.com>
2022-11-10 20:47:09 +0530

    added web scrapper

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cheerio = require('cheerio')
+const axios = require('axios')
+const PORT = 8000
+
+const app = express()
+const url = 'https://www.theguardian.com/uk'
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.fc-item__title', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles);
+    })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
